9f164989027c266fba5bd0015b73e8349c99d443
WhyN0t101 <80111844+WhyN0t101@users.noreply.github.com>
2021-03-14 23:35:03 +0000

    Add files via upload

---
+++ TestGui.java
@@ -0,0 +1,25 @@
+
+package testgui;
+
+
+
+public class TestGui {
+
+    
+    public static void main(String[] args) {
+       //JFrame frame = new JFrame();
+      /* frame.setTitle("Test Gui"); // Titulo
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // ação do botao close
+       frame.setResizable(false); // Impossibilita o Rezise da janela
+       frame.setSize(420,420); // tamanha da janela
+       frame.setVisible(true);
+       
+       ImageIcon image = new ImageIcon("E:\\Projects\\logo.png");
+       frame.setIconImage(image.getImage()); // muda o icon da janela
+       frame.getContentPane().setBackground(new Color(123,50,250)); // back ground color
+       
+      */ 
+       MyFrame myFrame = new MyFrame();
+    }
+    
+}

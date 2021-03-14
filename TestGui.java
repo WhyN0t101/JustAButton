4a926eafc098549ef8582c1dd393ff1aebaf17ca
WhyN0t101 <80111844+WhyN0t101@users.noreply.github.com>
2021-03-14 23:40:19 +0000

    Update TestGui.java

--- TestGui.java
+++ TestGui.java
@@ -9,14 +9,14 @@ public class TestGui {
     public static void main(String[] args) {
        //JFrame frame = new JFrame();
       /* frame.setTitle("Test Gui"); // Titulo
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // ação do botao close
-       frame.setResizable(false); // Impossibilita o Rezise da janela
-       frame.setSize(420,420); // tamanha da janela
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+       frame.setResizable(false); 
+       frame.setSize(420,420); 
        frame.setVisible(true);
        
        ImageIcon image = new ImageIcon("E:\\Projects\\logo.png");
-       frame.setIconImage(image.getImage()); // muda o icon da janela
-       frame.getContentPane().setBackground(new Color(123,50,250)); // back ground color
+       frame.setIconImage(image.getImage()); 
+       frame.getContentPane().setBackground(new Color(123,50,250)); 
        
       */ 
        MyFrame myFrame = new MyFrame();
